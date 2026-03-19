355019c1717d941e2168b6a8bff8c254f37f9c41
Bulka84 <98998260+Bulka84@users.noreply.github.com>
2026-03-19 19:36:38 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/adis",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
